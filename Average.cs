8bb50556f064757f7c50ceccbb108cd445f92f6a
Andrii Tretyakov <andeitretyakov@gmail.com>
2025-12-26 12:32:48 +0200

    First program for sort numbers

---
+++ Average.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project
+{
+    class Average
+    {
+        public static void Main()
+        {
+            List<int> numbers = new List<int> { 6, 1, 2, 10, 8 };
+            numbers.Add(8);
+            numbers.Remove(numbers[0]);
+            numbers.Sort();
+            
+
+            foreach(int el in numbers) {
+                Console.WriteLine("Numbers: " + el);
+            }
+        }
+    }
+}
+
+
+
+
